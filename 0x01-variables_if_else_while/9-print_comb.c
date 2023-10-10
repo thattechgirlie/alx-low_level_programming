6f983101268e98121601053461ffd6375ce55a9f
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-09 22:34:18 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,16 @@
  */
 int main(void)
 {
-	int y;
-	
-	for (y = 0; y < 10; y++)
+int y;
+for (y = 0; y < 10; y++)
+{
+	putchar(y + '0');
+	if (y != 9)
 	{
-		putchar(y + '0');
-		if (y != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(',');
+		putchar(' ');
 	}
-	putchar('\n');
-	return (0);
-
+}
+putchar('\n');
+return (0);
 }
