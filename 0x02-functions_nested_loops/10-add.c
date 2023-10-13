d95e6b2769c4c4dac0d00213f18ae5ba55bc461f
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-13 03:39:47 -0700

    times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - check the code
+ * @x: check parameter1
+ * @y: check parameter2
+ * Return: returns x + y.
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
+	
