2585cabc57224d0b0cf019e995bc78a6e1dea3ca
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-13 13:24:08 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ * @a: check parameter1
+ * @b: check parameter2
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
