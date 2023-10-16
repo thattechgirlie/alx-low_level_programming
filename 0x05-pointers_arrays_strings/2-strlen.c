c637c8ff101385c1bd77f980d7edf50503305289
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-16 13:06:34 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code
+ *@s: check parameter
+ * Return: returns count 
+ */
+int _strlen(char *s)
+{
+int a;
+int count = 0;
+
+for (a = 0; s[a] != '\0' ; a++)
+{
+	count ++;
+}
+return (count);
+}
