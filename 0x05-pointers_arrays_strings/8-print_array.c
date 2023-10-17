dd4f06a7e37811b4642d4326cf0435df053e91b2
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-17 00:45:42 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - check parameter
+ * @a: check parameter
+ * @n: check parameter1
+ * Return: always 0
+ */
+void print_array(int *a, int n)
+{
+int y;
+
+for (y = 0; y < n; y++)
+{
+	printf("%d", a[y]);
+	if (y < n - 1)
+	{
+		printf(". ");
+	}
+	printf("\n");
+}
+}
