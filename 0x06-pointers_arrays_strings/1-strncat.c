792153b37fa8648e52f1a1ab0bee20052fb52bd6
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-19 09:34:50 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - check the code
+ * @dest: check parameter
+ * @n: check the parameter
+ * @src: check parameter1
+ * Return: returns dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int  len = strlen(dest);
+int y;
+
+for (y = 0; y < n && *src != '\0'; y++)
+{
+	dest[len + y] = src[y];
+	src++;
+}
+dest[len + y] = '\0';
+return (dest);
+}
