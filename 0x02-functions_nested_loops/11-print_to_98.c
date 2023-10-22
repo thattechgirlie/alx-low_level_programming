69fcc409410b89129d1496c84a2b4acf3b93b610
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-22 05:41:15 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,11 @@ if (n >= 0 && n <= 98)
 {
 	for (y = n; y <= 98; y++)
 	{
-		printf("%d, ", y);
+		printf("%d", y);
+		if (y != 98)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
@@ -21,7 +25,11 @@ else if (n < 0)
 {
 	for (y = n; y <= 98; y++)
 	{
-		printf("%d, ", y);
+		printf("%d", y);
+ 		if (y != 98)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
@@ -29,13 +37,17 @@ else if (n > 98)
 {
 	for (y = n; y >= 98; y--)
 	{
-		printf("%d, ", y);
+		printf("%d", y);
+		if (y != 98)
+		{
+			 printf(", ");
+		}
 	}
 	printf("\n");
 }
 else if (n == 98)
 {
-	printf("%d, ", n);
+	printf("%d", n);
 	printf("\n");
 }
 }
