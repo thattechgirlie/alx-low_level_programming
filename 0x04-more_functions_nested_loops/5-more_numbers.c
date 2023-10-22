df9d30e7fe0edadb28ae671de94a1231bca07fea
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-22 08:36:35 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,13 @@
 void more_numbers(void)
 {
 int a, b;
-for (a = 1; a <= 10; a++)
+
+for (a = 0; a <= 9; a++)
 {
 	for (b = 0; b <= 14; b++)
 	{
-		if (b >= 10)
-		{
-			putchar('1');
-		}
+		if (b > 9)
+			putchar(b /10 + '0');
 		putchar(b % 10 + '0');
 	}
 	putchar('\n');
