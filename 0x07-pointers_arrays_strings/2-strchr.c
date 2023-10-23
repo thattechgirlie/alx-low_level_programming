f9847589ba3e26b8968d75af575a82da2bbdecc6
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-23 12:48:36 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - check the code
+ * @s: check parameter1
+ * @c: check parameter2
+ * Return: always void
+ */
+char *_strchr(char *s, char c)
+{
+int a;
+
+for (a = 0; s[a] >= '0'; a++)
+{
+	if (s[a] == c)
+	{
+		return (s + a);
+	}
+}
+return (0);
+}
