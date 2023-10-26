4d9a7f6ff0643e2994f5b4a1f612acd5fdb07b8b
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-26 03:40:20 -0700

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - check the code
+ * @s: check the parameter
+ * Return: returns void
+ */
+void _puts_recursion(char *s)
+{
+if (s)
+{
+	putchar(*s);
+	_puts_recursion(s +1);
+}
+else
+{
+	putchar('\n');
+}
+}
