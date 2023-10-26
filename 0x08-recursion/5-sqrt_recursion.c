2ea2fac8688f8eec8e9ef05155997d07c9284ecb
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-26 10:44:11 -0700

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _sqrt_recursion - check the code
+ * @n: check parameter
+ * Return: returns void
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+	return (-1);
+}
+else if (n == 0)
+	return (1);
+else
+{
+	return (_sqrt_recursion (n, 0));
+}
+}
