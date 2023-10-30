0998c8929b79440839d0e6692b02b82257773ae0
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-30 07:09:15 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - check code
+ * @argc: check count parameter
+ * @argv: check vector parameter
+ */
+int main (int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: check count parameter
+ * @argv: check vector parameter
+ * Return: returns void
+ */
+int main(int argc, char *argv[])
+{
