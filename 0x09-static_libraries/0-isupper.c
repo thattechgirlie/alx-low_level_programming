6fb0df512d97d7a52be3b68bc132aca65212cf75
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-30 03:24:31 -0700

    task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ * @c: check parameter
+ * Return: returns 0 or 1
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
