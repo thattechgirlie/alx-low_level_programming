978170a5f2cfcfe92d9977658705564d51db43d5
thattechgirlie <wanji.njoro1@gmai.com>
2023-10-31 11:56:16 -0700

    malloc 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - check the code
+ * @s1: check parameter1
+ * @s2: check parameter2
+ * Return: returns NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+
+
+
+
+}
