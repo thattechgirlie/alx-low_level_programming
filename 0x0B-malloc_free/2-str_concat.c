2fde17c3a9bd993dca86066313e94ef5865ba815
thattechgirlie <wanji.njoro1@gmai.com>
2023-11-01 09:06:04 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,8 +8,43 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+char *conct;
+int a, b;
 
+if (s1 == NULL)
+{
+	s1 = "";
+}
+if (s2 == NULL)
+{
+	s2 = "";
+}
+a = b = 0;
 
+while (s1[a] != '\0')
+{
+	a++;
+}
+while (s2[b] != '\0')
+{
+	b++;
+}
+conct = malloc(sizeof(char) * (a + b + 1));
 
+if (conct == NULL)
+	return (NULL);
+a = b = 0;
 
+while (s1[a] != '\0')
+{
+	conct[a] = s1[a];
+	a++;
+}
+while (s2[b] != '\0')
+{
+	conct[a] = s2[b];
+	a++, b++;
+}
+conct[a] = '\0';
+return (conct);
 }
