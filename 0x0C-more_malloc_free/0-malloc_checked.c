ede4bda272093593c12269a8c912c9779c99303d
thattechgirlie <wanji.njoro1@gmai.com>
2023-11-02 02:22:48 -0700

    malloc 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - check the code
+ * @b: check parameter
+ * Return: returns pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
+if (ptr == NULL)
+{
+	fprintf(stderr, "Memory allocation failed. Exiting...\n");
+	exit(EXIT_FAILURE);
+}
+return (ptr);
+}
