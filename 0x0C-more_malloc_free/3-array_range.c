fc9b1aff721ab9fe6dc671fd69e65b64d2662b2c
thattechgirlie <wanji.njoro1@gmai.com>
2023-11-03 07:12:21 -0700

    arrayrange

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,21 +10,24 @@
  */
 int *array_range(int min, int max)
 {
+int i;
+int size;
+int *array;
+
 if (min > max)
 {
 	return (NULL);
 }
-int size = max - min + 1;
-int *array = (int *)malloc(size * sizeof(int));
+size = max - min + 1;
+array = malloc(size * sizeof(int));
 
 if (array == NULL)
 {
-	fprintf(stderr, "Memory allocation failed. Exiting...\n");
-	exit(EXIT_FAILURE);
+	return (NULL);
 }
-for (int i = 0; i < size; i++)
+for (i = 0; min <= max; i++)
 {
-	array[i] = min + i;
+	array[i] = min ++;
 }
 return (array);
 }
