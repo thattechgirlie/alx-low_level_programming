225db52e7c323433d551c069e46737e6a907f32a
thattechgirlie <wanji.njoro1@gmai.com>
2023-11-06 09:36:23 -0800

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * free_dog - check the code
+ * @d: check the parameter
+ * Return: returns void
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
+}
