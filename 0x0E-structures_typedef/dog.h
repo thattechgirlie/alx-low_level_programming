e231047a363741e40217493b41b99f2b3a36aba7
thattechgirlie <wanji.njoro1@gmai.com>
2023-11-06 08:21:05 -0800

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - structure of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: returns void
+ */
 struct dog
 {
 	char *name;
