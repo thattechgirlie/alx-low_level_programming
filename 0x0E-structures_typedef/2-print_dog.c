ea1060d9da7f9a1663b13439251f49b1de492bc5
thattechgirlie <wanji.njoro1@gmai.com>
2023-11-06 08:08:15 -0800

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - check the code
+ * @d: chek parameter
+ * Return: returns void
+ */
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+	if (d == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d -> name);
+	if (d == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d -> owner);
+	if (d == NULL)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d -> age);
+}
+}
