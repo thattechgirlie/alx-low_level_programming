79e94861deed9b83956b33f7374a3931a17f662e
thattechgirlie <wanji.njoro1@gmai.com>
2023-11-06 08:26:44 -0800

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,13 @@
  * @owner: owner of dog
  * Return: returns void
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif
