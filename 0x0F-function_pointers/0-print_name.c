e23e662fa841b6b39cc5763a94aff8cf83befef4
thattechgirlie <wanji.njoro1@gmai.com>
2023-11-08 06:45:38 -0800

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - check he code
+ * @name: check paramter
+ * @f: check parameter1
+ * Return: returns void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL || f != NULL)
+{
+	f(name);
+}
+else
+{
+	return;
+}
+}
