e14d4e9a12e9ce4d96fb9abc67426a3b2b03df85
thattechgirlie <wanji.njoro1@gmai.com>
2023-11-09 05:05:39 -0800

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *main - check the code
  * @argc: check count parameter
@@ -7,14 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-if (argc == 3)
-{
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-}
-else
+if (argc < 3)
 {
 	printf("Error\n");
 	return (1);
 }
-return (0);
+else
+{
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
 }
