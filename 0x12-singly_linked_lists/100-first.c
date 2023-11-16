5bc1038dd6fef06462bca1eba7a5dabbec5dd2f1
thattechgirlie <wanji.njoro1@gmai.com>
2023-11-16 10:27:26 -0800

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: returns void
+ */
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
