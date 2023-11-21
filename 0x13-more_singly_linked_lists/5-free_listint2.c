baa488a7da9ccc6baa468f811e1e34e5b268c061
thattechgirlie <wanji.njoro1@gmail.com>
2023-11-21 02:00:00 -0800

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - check the code
+ * @head: check parameter
+ * Return: returns value
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *e;
+
+while (*head != NULL)
+{
+	e = (*head)->next;
+	free(*head);
+	*head = e;
+}
+*head = NULL;
+}
