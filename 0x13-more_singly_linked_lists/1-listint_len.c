8ebd49a00d5bb8bcd796688a6bc206750e0c62ad
thattechgirlie <wanji.njoro1@gmail.com>
2023-11-21 00:50:01 -0800

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,19 @@
 #include "lists.h"
 /**
- *
+ * listint_len - check the code
  * @h: check parameter
  * Return: returns number of nodes
  */
+size_t listint_len(const listint_t *h)
+{
+size_t a;
+a = 0;
+
+while (h != NULL)
+{
+	a++;
+	h = h->next;
+}
+return (a);
+}
 
