9458b2edf98a50f1770adecbd4ef1a6eb7be5e83
thattechgirlie <wanji.njoro1@gmail.com>
2023-11-24 11:04:41 -0800

    task2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * get_bit - check the code
+ * @n: check par1
+ * @index: check par2
+ * Return: value of bit at index or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned long int a;
+unsigned long int b;
+
+if (index > (sizeof(n) * 8 - 1))
+{
+	return (-1);
+}
+a = 1 << index;
+b = n & a;
+
+if (b == a)
+	return (0);
+return (0);
+}
