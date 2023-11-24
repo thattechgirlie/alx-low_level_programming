d36d674b813e50c35d28bc67876cef7bf9a65612
thattechgirlie <wanji.njoro1@gmail.com>
2023-11-24 11:11:48 -0800

    task two

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 unsigned long int a;
 unsigned long int b;
 
-if (index > (sizeof(n) * 8 - 1))
+if (index > (sizeof(unsigned long int) * 8 - 1))
 {
 	return (-1);
 }
